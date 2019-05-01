69438e33786f7fa3f96b6f485af1db1efcb909d7
Daniil Chizhevskij <daniilchizhevskij@gmail.com>
2019-05-01 14:00:02 +0300

    Initial commit

---
+++ data/settings.py
@@ -0,0 +1,21 @@
+##############################################################
+# Массив токенов сообщества с разрешением messages (от 1 шт) #
+##############################################################
+tokens = [
+	'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+	'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+	'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+	'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+]
+#################
+# ID сообщества #
+#################
+group_id = 123456789
+##################################################################
+# Нужно ли подствалять вместо подстроки '%s' имя пользователя #
+##################################################################
+send_name = True
+##################
+# Само сообщение #
+##################
+message = 'Привет, %s!'
