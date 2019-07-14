2c0be5ae59b761e93dfb571720f8b200e0aee516
Daniil Chizhevskii <enotprogrammist@mail.ru>
2019-07-14 18:56:31 +0300

    Update __init__.py

--- vkdelivery/__init__.py
+++ vkdelivery/__init__.py
@@ -6,9 +6,9 @@ session = vk.Session()
 api = vk.API(session, v=5.80)
 
 def get (**kwargs):
-	if ('tokens' not in kwargs.keys() or 'group_id' not in kwargs.keys()):
+	if 'tokens' not in kwargs.keys() or 'group_id' not in kwargs.keys():
 		raise KeyError ('One of the arguments is missing or invalid.')
-	if ('ui' not in kwargs.keys()):
+	if 'ui' not in kwargs.keys():
 		ui = False
 	else:
 		ui = kwargs['ui']
@@ -19,14 +19,14 @@ def get (**kwargs):
 		token_quantity = tokens.__len__()
 		number = api.messages.getDialogs(access_token=tokens[0], group_id=group_id)['count']
 		num = int(number / 200)
-		if (ui):
+		if ui:
 			print (gettime() + ' Users in database: ' + str(number))
 			print (gettime() + ' Gathering database...')
 			print (gettime() + ' Database gathering progress: 0% (0 of ' + str(number) + ').')
 		dialogs = []
 		percents = 0
 		for i in range(num):
-			if (token_number < token_quantity - 1):
+			if token_number < token_quantity - 1:
 				token_number = token_number + 1
 			else:
 				token_number =  0
@@ -36,11 +36,11 @@ def get (**kwargs):
 				user_id = messages['items'][j]['user_id']
 				if not (user_id in dialogs):
 					dialogs.append(user_id)
-				if (ui):
-					if (int((((i*200) + j + 1)) / number * 100) - 5 == percents):
+				if ui:
+					if int((((i*200) + j + 1)) / number * 100) - 5 == percents:
 						print(gettime() + ' Database gathering progress: ' + str(int((((i*200) + j + 1)) / number * 100)) + '% (' + str((i*200)+j + 1) + ' of ' + str(number) + ').')
 						percents = int(((i*200)+j + 1)/number*100)
-		if (token_number < token_quantity - 1):
+		if token_number < token_quantity - 1:
 			token_number = token_number + 1
 		else:
 			token_number =  0
@@ -50,11 +50,11 @@ def get (**kwargs):
 			user_id = messages['items'][i]['user_id']
 			if not (user_id in dialogs):
 				dialogs.append(user_id)
-			if (ui):
-				if (int((i + 1)/number) - 5 == percents):
+			if ui:
+				if int((i + 1)/number) - 5 == percents:
 					print(gettime() + ' Database progress: ' + str(int((i + 1)/number)) + '% (' + str(i + 1) + ' of ' + str(number) + ').')
 					percents = int((i + 1)/number)
-		if (ui):
+		if ui:
 			print (gettime() + ' Database had gathered successfully')
 		return dialogs
 	except vk.exceptions.VkAPIError as error:
@@ -63,9 +63,9 @@ def get (**kwargs):
 		raise SystemError('One of the arguments is invalid.')
 
 def send (**kwargs):
-	if ('tokens' not in kwargs.keys() or 'group_id' not in kwargs.keys() or 'dialogs' not in kwargs.keys() or 'message' not in kwargs.keys()):
+	if 'tokens' not in kwargs.keys() or 'group_id' not in kwargs.keys() or 'dialogs' not in kwargs.keys() or 'message' not in kwargs.keys():
 		raise KeyError ('One of the arguments is missing or invalid.')
-	if ('ui' not in kwargs.keys()):
+	if 'ui' not in kwargs.keys():
 		ui = False
 	else:
 		ui = kwargs['ui']
@@ -78,7 +78,7 @@ def send (**kwargs):
 		token_quantity = tokens.__len__()
 		percents = 0
 		num = dialogs.__len__()
-		if (ui):
+		if ui:
 			print (gettime() + ' Starting delivery...')
 			print (gettime() + ' Delivery progress: 0% (0 of ' + str(dialogs.__len__()) + ').')
 		for i in range (num):
@@ -91,11 +91,11 @@ def send (**kwargs):
 				api.messages.send(access_token=token, user_id=dialogs[i], message=message)
 			except vk.exceptions.VkAPIError:
 				continue
-			if (ui):
+			if ui:
 				if (int(((i + 1)/num) * 100) - 5 == percents):
 					print(gettime() + ' Database progress: ' + str(int(((i + 1)/num) * 100)) + '% (' + str(i + 1) + ' of ' + str(num) + ').')
 					percents = int(((i + 1)/num)*100)
-		if (ui):
+		if ui:
 			print (gettime() + ' Delivery had sent successfully')
 		return True
 	except vk.exceptions.VkAPIError as error:
@@ -104,9 +104,9 @@ def send (**kwargs):
 		raise SystemError('One of the arguments is invalid.')
 
 def getandsend (**kwargs):
-	if ('tokens' not in kwargs.keys() or 'group_id' not in kwargs.keys() or 'message' not in kwargs.keys()):
+	if 'tokens' not in kwargs.keys() or 'group_id' not in kwargs.keys() or 'message' not in kwargs.keys():
 		raise KeyError ('One of the arguments is missing or invalid.')
-	if ('ui' not in kwargs.keys()):
+	if 'ui' not in kwargs.keys():
 		ui = False
 	else:
 		ui = kwargs['ui']
